d66b1cb0592c5003697c7b4dc7e350d12e56fd6d
Akshay Phargade <phargadeakshay@gmail.com>
2023-02-05 11:15:22 +0530

    navbar created

---
+++ src/Slices/ProductListSlice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductListSlice = () => {
+  return (
+    <div>ProductListSlice</div>
+  )
+}
+
+export default ProductListSlice
---
+++ src/components/Navbar.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+  const [navbar, setNavbar] = useState(false);
+  const dispatch = useDispatch();
+  return (
+    <nav className="w-full bg-red-500 shadow">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-3 md:block">
+            <Link className="xxsm:text-sm text-2xl font-bold text-white" to="/">
+              Logo
+            </Link>
+            <div className="md:hidden flex">
+              <Link
+                className="flex items-center hover:text-violet-500-200 px-4 py-2 text-white rounded-md shadow hover:bg-violet-500"
+                to="/cart"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 md:inline-block"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+              </Link>
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none "
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <h1>❌</h1>
+                ) : (
+                  <div className="flex items-center gap-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </div>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-white hover:text-indigo-200">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="text-white hover:text-indigo-200">
+                <a href="#">Contact US</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+       {/*  */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
